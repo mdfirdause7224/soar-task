c7e9b89334ab5dd7b24a6ff36510e5280f344007
mdfirdause7224 <mdfirdause7224@gmail.com>
2025-03-15 00:29:50 +0530

    chore: changed

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,56 +1,56 @@
-// function SideBar() {
-//   return (
-//     <>
-//       <div className="bg-white w-[250px] h-[700px] py-3">
-//         <div className="flex p-4 px-8 gap-2 font-extrabold text-2xl text-blue-900 hover:text-black font-semibold">
-//           <img src="./downloads.svg" alt="opacity-60" />
-//           Soar Task
-//         </div>
-//         <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
-//           <img className="opacity-60 hover:opacity-100" src="./download (1).svg" alt="opacity-60" />
-//           Dashboard
-//         </div>
-//         <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
-//           <img className="opacity-60 hover:opacity-100" src="./download (2).svg" alt="opacity-60" />
-//           Transactions
-//         </div>
-//         <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
-//           <img className="opacity-60 hover:opacity-100" src="./download (3).svg" alt="opacity-60" />
-//           Accounts
-//         </div>
-//         <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
-//           <img className="opacity-60 hover:opacity-100" src="./download (4).svg" alt="opacity-60" />
-//           Investments
-//         </div>
-//         <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
-//           <img className="opacity-60 hover:opacity-100" src="./9.svg" alt="opacity-60" />
-//           Credit Cards
-//         </div>
-//         <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
-//           <img className="opacity-60 hover:opacity-100" src="./download (5).svg" alt="opacity-60" />
-//           Loans
-//         </div>
-//         <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
-//           <img className="opacity-60 hover:opacity-100" src="./download (6).svg" alt="opacity-60" />
-//           Services
-//         </div>
-//         <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
-//           <img className="opacity-60 hover:opacity-100" src="./download (7).svg" alt="opacity-60" />
-//           My Privileges
-//         </div>
-//         <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
-//           <img className="opacity-60 hover:opacity-100" src="./download (8).svg" alt="opacity-60" />
-//           Setting
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default SideBar
-
 function SideBar() {
-  return <div></div>;
+  return (
+    <>
+      <div className="bg-white w-[250px] h-[700px] py-3">
+        <div className="flex p-4 px-8 gap-2 font-extrabold text-2xl text-blue-900 hover:text-black font-semibold">
+          <img src="./downloads.svg" alt="opacity-60" />
+          Soar Task
+        </div>
+        <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
+          <img className="opacity-60 hover:opacity-100" src="./download (1).svg" alt="opacity-60" />
+          Dashboard
+        </div>
+        <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
+          <img className="opacity-60 hover:opacity-100" src="./download (2).svg" alt="opacity-60" />
+          Transactions
+        </div>
+        <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
+          <img className="opacity-60 hover:opacity-100" src="./download (3).svg" alt="opacity-60" />
+          Accounts
+        </div>
+        <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
+          <img className="opacity-60 hover:opacity-100" src="./download (4).svg" alt="opacity-60" />
+          Investments
+        </div>
+        <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
+          <img className="opacity-60 hover:opacity-100" src="./9.svg" alt="opacity-60" />
+          Credit Cards
+        </div>
+        <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
+          <img className="opacity-60 hover:opacity-100" src="./download (5).svg" alt="opacity-60" />
+          Loans
+        </div>
+        <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
+          <img className="opacity-60 hover:opacity-100" src="./download (6).svg" alt="opacity-60" />
+          Services
+        </div>
+        <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
+          <img className="opacity-60 hover:opacity-100" src="./download (7).svg" alt="opacity-60" />
+          My Privileges
+        </div>
+        <div className="flex p-4 px-10 gap-5 text-gray-400 hover:text-black font-semibold">
+          <img className="opacity-60 hover:opacity-100" src="./download (8).svg" alt="opacity-60" />
+          Setting
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SideBar;
+
+// function SideBar() {
+//   return <div></div>;
+// }
+
+// export default SideBar;
