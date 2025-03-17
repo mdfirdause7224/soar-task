10a9260896458b956c8393b9ba6e745632e74929
mdfirdause7224 <mdfirdause7224@gmail.com>
2025-03-17 15:22:38 +0530

    chore: some changes are done

---
+++ src/components/RecentTransactions.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryFn } from '@storybook/react';
+import React from 'react';
+import RecentTransactions from '../components/RecentTransactions';
+
+export default {
+  title: 'Components/RecentTransactions',
+  component: RecentTransactions,
+  argTypes: {
+    date: { control: 'text' },
+    transaction: { control: 'boolean' },
+    message: { control: 'text' },
+    amount: { control: 'number' },
+    image: { control: 'text' }
+  }
+} as Meta;
+
+type RecentTransactionsProps = React.ComponentProps<typeof RecentTransactions>;
+
+const Template: StoryFn<RecentTransactionsProps> = (args) => <RecentTransactions {...args} />;
+
+export const PositiveTransaction = Template.bind({});
+PositiveTransaction.args = {
+  date: '2025-03-17',
+  transaction: true,
+  message: 'Payment Received',
+  amount: 100,
+  image: 'https://avatar.iran.liara.run/public?4'
+};
+
+export const NegativeTransaction = Template.bind({});
+NegativeTransaction.args = {
+  date: '2025-03-16',
+  transaction: false,
+  message: 'Purchase at Store',
+  amount: 50,
+  image: 'https://avatar.iran.liara.run/public?3'
+};
